774ef93e1edd8b5f7c9b17fa17dee9ef3b1f1b11
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-12 10:18:33 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from .utils.database import db
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    
+    # Initialize database
+    db.init_app(app)
+    
+    # Register API routes
+    from .api.voice_response import voice_response_bp
+    app.register_blueprint(voice_response_bp)
+    
+    return app
