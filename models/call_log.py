77b899114b9b98a22acf2351cd5de3838521bc1d
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-12 10:21:41 +0530

    Update call_log.py

--- models/call_log.py
+++ models/call_log.py
@@ -1,1 +1,8 @@
+from app.utils.database import db
 
+class CallLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    caller_number = db.Column(db.String(20), nullable=False)
+    call_time = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    transcript = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=False)
