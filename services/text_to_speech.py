9f910997275a5a6eaf022869797c6becda41eda2
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-12 10:21:21 +0530

    Update text_to_speech.py

--- services/text_to_speech.py
+++ services/text_to_speech.py
@@ -1,1 +1,7 @@
-text_to_speech.py
+from gtts import gTTS
+import os
+
+def text_to_speech(text, output_file="response.mp3"):
+    tts = gTTS(text=text, lang='en')
+    tts.save(output_file)
+    return output_file
